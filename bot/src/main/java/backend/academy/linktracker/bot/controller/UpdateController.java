301abbdfd290315483e0adfddf0f10b9739a4757
iimukminov <iimukminov06@gmail.com>
2026-03-21 20:15:55 +0300

    corrected the comments 2.75

--- bot/src/main/java/backend/academy/linktracker/bot/controller/UpdateController.java
+++ bot/src/main/java/backend/academy/linktracker/bot/controller/UpdateController.java
@@ -3,8 +3,10 @@ package backend.academy.linktracker.bot.controller;
 import backend.academy.linktracker.bot.api.UpdatesApi;
 import backend.academy.linktracker.bot.dto.LinkUpdate;
 import backend.academy.linktracker.bot.service.BotUpdateService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +16,11 @@ public class UpdateController implements UpdatesApi {
     private final BotUpdateService botUpdateService;
 
     @Override
-    public ResponseEntity<Void> updatesPost(LinkUpdate linkUpdate) {
+    public ResponseEntity<Void> updatesPost(@Valid @RequestBody LinkUpdate linkUpdate) {
+        if (linkUpdate.getUrl() == null || linkUpdate.getId() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
         botUpdateService.processUpdate(linkUpdate);
         return ResponseEntity.ok().build();
     }
